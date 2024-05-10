f2ff738a9dce0621ae1d7b92a90be53530c2ce44
nazeani <tu7mp4w99@code.edu.az>
2024-05-10 19:52:02 +0400

    first commit

---
+++ 8may/Areas/Admin/Controllers/FeatureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _8may.Areas.Admin.Controllers
+{
+    public class FeatureController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pronia.Business/Services/Abstracts/IFeatureService.cs
@@ -0,0 +1,18 @@
+﻿using Pronia.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pronia.Business.Services.Abstracts
+{
+    public interface IFeatureService
+    {
+        void Add(Feature feature);
+        void Delete(int id);
+        void Update(int id, Feature feature);
+        Feature Get(Func<Feature, bool> func = null);
+        List<Feature> GetAll(Func<Feature, bool> func = null);
+    }
+}
